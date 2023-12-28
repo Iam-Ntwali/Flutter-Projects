cae90f43683358e598aeee21d62e09a2f0b31f5d
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2023-12-28 20:50:17 +0200

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,11 @@ void main() {
           ),
         ),
         body: const Center(
-          child: Image(
-            image: NetworkImage(
-              'https://images.unsplash.com/photo-1526779259212-939e64788e3c?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            ),
+            child: Image(
+          image: AssetImage(
+            'assets/diamond.png',
           ),
-        ),
+        )),
       ),
     ),
   );
