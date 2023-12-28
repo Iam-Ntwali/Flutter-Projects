db37968fed191e91133d75b5a8fd1b49551da157
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2023-12-28 20:39:31 +0200

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,16 @@ void main() {
           backgroundColor: Colors.blueGrey[900],
           title: const Text(
             'My App',
-            style: TextStyle(color: Colors.grey),
+            style: TextStyle(
+              color: Colors.grey,
+            ),
+          ),
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+              'https://images.unsplash.com/photo-1526779259212-939e64788e3c?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            ),
           ),
         ),
       ),
