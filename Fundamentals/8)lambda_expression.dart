49f52ab2ca776cad9de1f302cb449e24d7af368d
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2023-12-29 14:39:59 +0200

    some changes

--- Fundamentals/8)lambda_expression.dart
+++
@@ -1,9 +0,0 @@
-// Lambda is like anonymous functions in Javascript,
-
-void main() {
-  final numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-
-  for (var num in numbers) {
-    print(num - 1);
-  }
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          backgroundColor: Colors.blueGrey[900],
-          title: const Text(
-            'My App',
-            style: TextStyle(color: Colors.grey),
-          ),
-        ),
-        body: const Center(
-          child: Image(
-            image: AssetImage('assets/diamond.png'),
-          ),
-        ),
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.orange[100],
+      appBar: AppBar(
+        title: Text('My First Title'),
+        backgroundColor: Colors.pinkAccent,
       ),
     ),
-  );
+  ));
 }
