34b6f23d38fdd69f55f65fcf01f104fc28aa28af
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2023-12-29 18:59:13 +0200

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,49 @@
+// ignore_for_file: unnecessary_const
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      backgroundColor: Colors.orange[100],
-      appBar: AppBar(
-        title: Text('My First Title'),
-        backgroundColor: Colors.pinkAccent,
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue[100],
+        // appBar: AppBar(
+        //   title: const Text(
+        //     'My First App',
+        //     style: TextStyle(
+        //       fontWeight: FontWeight.w600,
+        //     ),
+        //   ),
+        //   backgroundColor: Colors.blueAccent,
+        // ),
+        body: const SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage(
+                  'assets/3d-hygge.png',
+                ),
+              ),
+              Text(
+                'Ntwali 🇷🇼',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  ));
+    );
+  }
 }
